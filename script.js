e1416d8261b39a39b249751321642a23a77fad8e
Chinmay Gaidhane <296240652+chinmaygaidhane-svg@users.noreply.github.com>
2026-09-23 22:41:13 +0530

    added js code

---
+++ script.js
@@ -0,0 +1,30 @@
+const display = document.getElementById("display")
+// display.value="67"
+// console.log(display.value)
+const buttons = document.querySelectorAll(".buttons button")
+// console.Log(buttons.length)
+function press(value){
+    if (value ==='C'){
+        display.value='';
+
+    }else if(value === 'DEL'){
+        display.value=display.value.slice(0,-1);
+
+    }else if(value ==='='){
+        calculate()
+    }
+    else{
+        display.value = display.value + value;
+    }
+}
+
+function calculate(){
+    const answer =eval(display.value)
+    display.value=answer
+
+}
+for(let i=0;i<buttons.length;i++){
+    buttons[i].addEventListener('click',function(){
+        press(buttons[i].textContent)
+    })
+}
